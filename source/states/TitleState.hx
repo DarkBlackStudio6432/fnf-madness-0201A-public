29b4f1a7e63a3383ee11a85206fb7c08ea617f35
DarkBlackStudio6432 <95198141+DarkBlackStudio6432@users.noreply.github.com>
2026-01-13 16:52:49 -0300

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -10,15 +10,12 @@ import flixel.util.FlxColor;
 import flixel.util.FlxMath;
 import flixel.input.keyboard.FlxKey;
 import flixel.addons.transition.FlxTransitionableState;
-import flixel.graphics.frames.FlxAtlasFrames;
-import flixel.graphics.frames.FlxFrame;
 import flixel.group.FlxGroup;
 import flixel.input.gamepad.FlxGamepad;
 import flixel.sound.FlxSound;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
 
-import haxe.Json;
 import openfl.Assets;
 
 import shaders.ColorSwap;
@@ -42,15 +39,10 @@ typedef TitleData =
 
 class TitleState extends MusicBeatState
 {
-	public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO];
-	public static var volumeDownKeys:Array<FlxKey> = [FlxKey.NUMPADMINUS, FlxKey.MINUS];
-	public static var volumeUpKeys:Array<FlxKey> = [FlxKey.NUMPADPLUS, FlxKey.PLUS];
-
 	public static var initialized:Bool = false;
 
 	var blackScreen:FlxSprite;
 	var credGroup:FlxGroup;
-	var credTextShit:Alphabet;
 	var textGroup:FlxGroup;
 	var ngSpr:FlxSprite;
 
@@ -58,108 +50,59 @@ class TitleState extends MusicBeatState
 	var titleTextAlphas:Array<Float> = [1, .64];
 
 	var curWacky:Array<String> = [];
-	var wackyImage:FlxSprite;
-
 	var mustUpdate:Bool = false;
 	var titleJSON:TitleData;
-
-	public static var updateVersion:String = '';
 
 	override public function create():Void
 	{
 		Paths.clearStoredMemory();
-
-		#if LUA_ALLOWED
-		Mods.pushGlobalMods();
-		#end
-		Mods.loadTopMod();
-
-		FlxG.fixedTimestep = false;
-		FlxG.game.focusLostFramerate = 60;
-
-		#if !FLX_NO_KEYBOARD
-		FlxG.keys.preventDefaultKeys = [TAB];
-		#end
-
-		curWacky = FlxG.random.getObject(getIntroTextShit());
-
 		super.create();
 
 		FlxG.save.bind('funkin', CoolUtil.getSavePath());
 		ClientPrefs.loadPrefs();
-
 		Highscore.load();
 
 		titleJSON = tjson.TJSON.parse(Paths.getTextFromFile('images/gfDanceTitle.json'));
 
 		if(!initialized)
 		{
-			if(FlxG.save.data != null && FlxG.save.data.fullscreen)
-				FlxG.fullscreen = FlxG.save.data.fullscreen;
-
 			persistentUpdate = true;
 			persistentDraw = true;
 		}
-
-		if (FlxG.save.data.weekCompleted != null)
-			StoryMenuState.weekCompleted = FlxG.save.data.weekCompleted;
 
 		#if !FLX_NO_MOUSE
 		FlxG.mouse.visible = false;
 		#end
 
-		if (initialized)
-			startIntro();
-		else
-			new FlxTimer().start(1, function(_) startIntro());
+		startIntro();
+		initialized = true;
 	}
 
 	var logoBl:FlxSprite;
 	var gfDance:FlxSprite;
-	var danceLeft:Bool = false;
 	var titleText:FlxSprite;
-	var swagShader:ColorSwap = null;
 
 	function startIntro()
 	{
-		if (!initialized && FlxG.sound.music == null)
+		if (FlxG.sound.music == null)
 			FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 
 		Conductor.bpm = titleJSON.bpm;
-		persistentUpdate = true;
 
-		var bg:FlxSprite = new FlxSprite();
-		bg.antialiasing = ClientPrefs.data.antialiasing;
-
-		if (titleJSON.backgroundSprite != null && titleJSON.backgroundSprite != "none")
-			bg.loadGraphic(Paths.image(titleJSON.backgroundSprite));
-		else
-			bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-
+		var bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
 		logoBl = new FlxSprite(titleJSON.titlex, titleJSON.titley);
 		logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
-		logoBl.antialiasing = ClientPrefs.data.antialiasing;
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
 		logoBl.animation.play('bump');
 		add(logoBl);
-
-		if(ClientPrefs.data.shaders)
-			swagShader = new ColorSwap();
 
 		gfDance = new FlxSprite(titleJSON.gfx, titleJSON.gfy);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByPrefix('danceLeft', 'gfDance', 24, false);
 		gfDance.animation.addByPrefix('danceRight', 'gfDance', 24, false);
-		gfDance.antialiasing = ClientPrefs.data.antialiasing;
 		add(gfDance);
-
-		if(swagShader != null)
-		{
-			gfDance.shader = swagShader.shader;
-			logoBl.shader = swagShader.shader;
-		}
 
 		titleText = new FlxSprite(titleJSON.startx, titleJSON.starty);
 		titleText.frames = Paths.getSparrowAtlas('titleEnter');
@@ -167,8 +110,6 @@ class TitleState extends MusicBeatState
 		titleText.animation.addByPrefix('press', "ENTER PRESSED", 24);
 		titleText.animation.play('idle');
 		add(titleText);
-
-		initialized = true;
 	}
 
 	override function update(elapsed:Float)
